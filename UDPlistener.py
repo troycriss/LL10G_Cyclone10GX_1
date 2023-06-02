6de1140b37ba8808363c320ec4ebcc4222e49e39
drandyhaas <drandyhaas@gmail.com>
2023-06-02 12:00:29 -0400

    Print only 1/100 packets. This keeps up with nbitstosend==1 traffic on 10G.

--- UDPlistener.py
+++ UDPlistener.py
@@ -31,10 +31,12 @@ npacket=0
 while True:
     data, address = s.recvfrom(1520)
     npacket=npacket+1
-    if npacket%100==0: print("Got packet", npacket)
-    seq_num=data[:2] # sequence id of packet sent
-    packet_tx_count=data[2:4] # number of packets sent
-    fifo_rdusedw=data[4:6] # fifo used length at time of sending
-    print(repr(address), int.from_bytes(seq_num,"big"), int.from_bytes(packet_tx_count,"big"), int.from_bytes(fifo_rdusedw,"big"))
-    data=data[6:]
-    print(len(data),"bytes:", ' '.join('{:02x}'.format(b) for b in data))
+    if npacket%100==0:
+        print("Got packet", npacket)
+    if npacket%100==0:
+        seq_num=data[:2] # sequence id of packet sent
+        packet_tx_count=data[2:4] # number of packets sent
+        fifo_rdusedw=data[4:6] # fifo used length at time of sending
+        print(repr(address), int.from_bytes(seq_num,"big"), int.from_bytes(packet_tx_count,"big"), int.from_bytes(fifo_rdusedw,"big"))
+        data=data[6:]
+        print(len(data),"bytes:", ' '.join('{:02x}'.format(b) for b in data))
