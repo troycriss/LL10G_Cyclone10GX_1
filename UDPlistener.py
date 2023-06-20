96d08911009867954196de4fed0b2252c1f89f35
andredubovski <andre@dubov.ski>
2023-06-20 15:21:31 -0400

    bit parser works quickly now, with struct.unpack()

--- UDPlistener.py
+++ UDPlistener.py
@@ -2,7 +2,8 @@
 
 import socket
 import statistics
-import struct 
+import struct
+
 SEND_BUF_SIZE = 65536*1000
 RECV_BUF_SIZE = 65536*1000
 def modify_buff_size(sock):
@@ -30,9 +31,22 @@ server_address = ("", 2023)
 s.bind(server_address)
 print("Ctrl+c to exit the program!")
 npacket=0
-trigger_signal_offset=7*4
+trigger_signal_offset=8*4
 
 saved_signal = [0]  # List to store selected signal bits
+
+def IK_RemoveOddBits(x):
+
+    x = ((x & 0x44444444) >> 1) | ((x & 0x11111111) >> 0);
+    x = ((x & 0x30303030) >> 2) | ((x & 0x03030303) >> 0);
+    x = ((x & 0x0F000F00) >> 4) | ((x & 0x000F000F) >> 0);
+    x = ((x & 0x00FF0000) >> 8) | ((x & 0x000000FF) >> 0);
+
+    return x;
+
+
+def IK_RemoveEvenBits(x):
+    return IK_RemoveOddBits(x >> 1);
 
 while True:
     data, address = s.recvfrom(1520)
@@ -48,25 +62,21 @@ while True:
         
         j=0  
         trigger_counter=-1
+        words = struct.unpack('Q'*int(len(data)/8), data)
         
-        for byte in data:
-            
-            for i in range(8):
-                bit = (byte >> i) & 1  # Extract each bit   
-                            
-                if i % 2 == 0:
-                    if bit == 1:
-                        trigger_counter=trigger_signal_offset
-                    else:
-                        trigger_counter=trigger_counter-1
-                else:
-                    if trigger_counter == 0:
-                        saved_signal.append(bit)
-             
-            #print(j,": trig:",trigger, "sig:",signal)
-            j+=1
+        old_trigger = 1
+        for word in words:
+            bit = 0
+            if old_trigger == 0:
+                continue
+            else:
+                bit = (old_trigger & IK_RemoveEvenBits(word)) >> (old_trigger.bit_length()-1)
+            saved_signal.append(bit)
+            #print(old_trigger, ": ", bit)
+            old_trigger = IK_RemoveOddBits(word)
         
-        if npacket%100==0:
+        
+        if npacket%10000==0:
             print(npacket, statistics.mean(saved_signal))
             saved_signal = [0]  # List to store selected signal bits
 
