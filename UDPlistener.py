627d3916170565ecab92498aa30805d5af8c30aa
andredubovski <andre@dubov.ski>
2023-07-06 12:52:46 -0400

    improved and simplified UDPlistner.py

--- UDPlistener.py
+++ UDPlistener.py
@@ -3,6 +3,7 @@
 import socket
 import statistics
 import struct
+from bitarray import bitarray
 
 SEND_BUF_SIZE = 65536*1000
 RECV_BUF_SIZE = 65536*1000
@@ -33,21 +34,18 @@ print("Ctrl+c to exit the program!")
 npacket=0
 trigger_signal_offset=8*4
 
-saved_signal = [0]  # List to store selected signal bits
+saved_signal = bitarray()  # List to store selected signal bits
 
-def IK_RemoveOddBits(x):
+def oddBits(x):
+    y = (x & 0x5555555555555555)
+    return y
+    
+def evenBits(x):
+    return oddBits(x >> 1)
 
-    x = ((x & 0x44444444) >> 1) | ((x & 0x11111111) >> 0);
-    x = ((x & 0x30303030) >> 2) | ((x & 0x03030303) >> 0);
-    x = ((x & 0x0F000F00) >> 4) | ((x & 0x000F000F) >> 0);
-    x = ((x & 0x00FF0000) >> 8) | ((x & 0x000000FF) >> 0);
-
-    return x;
-
-
-def IK_RemoveEvenBits(x):
-    return IK_RemoveOddBits(x >> 1);
-
+f = open("data4.txt", "a")
+printEveryNPackets = 10000
+totalDataPoints = 0
 while True:
     data, address = s.recvfrom(1520)
     npacket=npacket+1
@@ -60,26 +58,24 @@ while True:
         #print(repr(address), int.from_bytes(seq_num,"big"), int.from_bytes(packet_tx_count,"big"), int.from_bytes(fifo_rdusedw,"big"))
         data=data[6:]
         
-        j=0  
-        trigger_counter=-1
         words = struct.unpack('Q'*int(len(data)/8), data)
+        #print(words, '\n')
         
-        old_trigger = 1
+        old_trigger = 0
         for word in words:
-            bit = 0
-            if old_trigger == 0:
-                continue
-            else:
-                bit = (old_trigger & IK_RemoveEvenBits(word)) >> (old_trigger.bit_length()-1)
-            saved_signal.append(bit)
-            #print(old_trigger, ": ", bit)
-            old_trigger = IK_RemoveOddBits(word)
+            
+            old_trigger = old_trigger >> 36
+            if old_trigger != 0:
+                bit = (old_trigger & evenBits(word)) >> (old_trigger.bit_length()-1)
+                saved_signal.append(bit)
+                
+            old_trigger = oddBits(word)
+            #print(bin(oddBits(word)), ": ", bin(evenBits(word)))
         
-        
-        if npacket%10000==0:
-            print(npacket, statistics.mean(saved_signal))
-            saved_signal = [0]  # List to store selected signal bits
-
-        #print('Selected Signal bits:')
-        #print(' '.join('{:2b}'.format(b) for b in saved_signal))
-        
+        if npacket%printEveryNPackets==0:
+            f.write(saved_signal.to01())
+            totalDataPoints+=len(saved_signal)
+            #print('Received {} packets'.format(printEveryNPackets))
+            print('Received:\n\tPacket number: {}\n\tTotal data points: {}\n\tBias of the last {} packets: {}\n'.format(npacket, totalDataPoints, printEveryNPackets, statistics.mean(saved_signal)))#, saved_signal)
+            #print(saved_signal)
+            saved_signal = bitarray()  # List to store selected signal bits
