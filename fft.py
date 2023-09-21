2e369edad0ecd8d4574f4edab7eecebc68d8b1cc
Andy <drandyhaas@gmail.com>
2023-09-21 12:15:54 -0400

    initial

--- fft.py
+++ fft.py
@@ -1,10 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#read in data
+bytestream = bytearray(b'')
+with open('megabyte.txt', 'rb') as file:
+    for line in file:
+        for b in line:
+            bytestream.append(b)
+boolstream = []
+for b in bytestream:
+    for i in range(8):
+        boolstream.append((b >> i) & 0b1)
+boolstream=boolstream[200000:300000]
+print(boolstream[:100])
+
 # sampling rate, number of samples per second
-sr = 100
+sr = 1/303e-9
+print("sr",sr)
 #number of samples
-ns = 1000
+ns = len(boolstream)
+print("ns",ns)
 
 # sampling period
 ts = 1.0/sr
@@ -12,13 +27,14 @@ ts = 1.0/sr
 t = np.arange(0,ns*ts,ts)
 
 # make fake data
-x = 0*t
+#x = 0*t
 #x += 1.0* (np.sin(2*np.pi*4*t)+1)
-x += 0.5* 1 * (np.sin(2*np.pi*7*t)+1)
-x=np.round(x)
-
+#x += 0.5* 1 * (np.sin(2*np.pi*7*t)+1)
 #x[40:50]=17
+# #x=np.round(x)
 #print(np.average(x))
+
+x=boolstream
 
 plt.plot(t, x, 'r')
 plt.xlabel('Time')
@@ -35,10 +51,12 @@ plt.stem(freq, np.abs(X), 'b',markerfmt=" ")
 plt.xlabel('Freq (Hz)')
 plt.ylabel('FFT Amplitude |X(freq)|')
 plt.xlim(1, freq[-1]/2.) # ignore 0 freq (DC offset), and only go up to half the max freq, since it's symmetric
-plt.ylim(.01,1.25*max(np.abs(X[1:])))
-plt.yscale("log")
+plt.ylim(0,1.05*max(np.abs(X[1:])))
+#plt.ylim(.01,1.25*max(np.abs(X[1:]))); plt.yscale("log")
+print(  freq[np.where( np.abs(X[1:])==max(np.abs(X[1:int(ns/2)])) )[0]]  )
 plt.show()
 
+exit()
 
 ifftX = ifft(X)
 plt.plot(t, np.real(ifftX), 'r')
