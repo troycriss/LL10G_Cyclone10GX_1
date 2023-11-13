51b063a5b6defd1555fda5d8c079f3486835ac50
andredubovski <andre@dubov.ski>
2023-11-13 12:12:50 -0500

    added console command for offset to check for reset error

--- pulse/PulseController.v
+++ pulse/PulseController.v
@@ -52,7 +52,7 @@ always@(posedge clk_in)
 //(R)eset then (W)rite then (M)easure, then do it again with reversed polarity for R and W
 //M pulse is always positive
 begin
-	pulse_durations[15] <= pos1dur[maxl:0]; 			//R+
+	pulse_durations[7] <= pos1dur[maxl:0]; 			//R+
 	pulse_durations[0]  <= pos1pausedur[maxl:0];	//pause
 	pulse_durations[1]  <= pos2dur[maxl:0]; 			//M
 	pulse_durations[2]  <= pos2pausedur[maxl:0];	//pause
@@ -61,14 +61,14 @@ begin
 	pulse_durations[5]  <= pos4dur[maxl:0]; 			//M
 	pulse_durations[6]  <= pos4pausedur[maxl:0];	//pause
 	
-	pulse_durations[7]  <= neg1dur[maxl:0]; 			//R-
-	pulse_durations[8]  <= neg1pausedur[maxl:0];	//pause
-	pulse_durations[9]  <= neg2dur[maxl:0]; 	   	//M
-	pulse_durations[10] <= neg2pausedur[maxl:0];	//pause
-	pulse_durations[11] <= neg3dur[maxl:0]; 			//W+
-	pulse_durations[12] <= neg3pausedur[maxl:0];	//pause
-	pulse_durations[13] <= neg4dur[maxl:0]; 	   	//M
-	pulse_durations[14] <= neg4pausedur[maxl:0];	//pause
+//	pulse_durations[7]  <= neg1dur[maxl:0]; 			//R-
+//	pulse_durations[8]  <= neg1pausedur[maxl:0];	//pause
+//	pulse_durations[9]  <= neg2dur[maxl:0]; 	   	//M
+//	pulse_durations[10] <= neg2pausedur[maxl:0];	//pause
+//	pulse_durations[11] <= neg3dur[maxl:0]; 			//W+
+//	pulse_durations[12] <= neg3pausedur[maxl:0];	//pause
+//	pulse_durations[13] <= neg4dur[maxl:0]; 	   	//M
+//	pulse_durations[14] <= neg4pausedur[maxl:0];	//pause
 end
 
 
@@ -82,65 +82,65 @@ always@(posedge clk_in)
 		case (pulse_index)
 		
 			4'd0: begin
-				signal_out<=8'b1000_1001;	//R+
+				signal_out<=8'b0000_0010;	//R+
 			end
 			4'd1: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 				is_new_pulse <= 1'b1;
 			end
 			
 			4'd2: begin
-				signal_out<=8'b1000_0101;	//M
+				signal_out<=8'b0000_0100;	//M
 				if(is_new_pulse) verify_trigger<=1'b1;
 				is_new_pulse <= 1'b0;
 			end
 			4'd3: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 			end
 			
 			4'd4: begin
-				signal_out<=8'b1001_0001;	//W-
+				signal_out<=8'b0001_0000;	//W-
 			end
 			4'd5: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 				is_new_pulse <= 1'b1;
 			end
 			
 			4'd6: begin
-				signal_out<=8'b1000_0101;	//M
+				signal_out<=8'b0000_0001;	//M
 				if(is_new_pulse) trigger<=1'b1;
 				is_new_pulse <= 1'b0;
 			end
 			4'd7: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 			end
 			
 			//now the same pulses but with R and W upside down
 			
 			4'd8: begin
-				signal_out<=8'b1010_0001;	//R-
+				signal_out<=8'b0010_0000;	//R-
 			end
 			4'd9: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 			end
 			
 			4'd10: begin
-				signal_out<=8'b1000_0101;	//M
+				signal_out<=8'b0000_0100;	//M
 				//if(is_new_pulse) trigger<=1'b1;
 			end
 			4'd11: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 			end
 			
 			4'd12: begin
-				signal_out<=8'b1000_0011;	//W+
+				signal_out<=8'b0000_0010;	//W+
 			end
 			4'd13: begin
-				signal_out<=8'b1000_0001;
+				signal_out<=8'b0000_0000;
 			end
 			
 			4'd14: begin
-				signal_out<=8'b1000_0101;	//M
+				signal_out<=8'b0000_0100;	//M
 				//if(is_new_pulse) trigger<=1'b1;
 			end
 			4'd15: begin
@@ -154,7 +154,7 @@ always@(posedge clk_in)
 		
 		if(timer == currentpuldur) begin
 			currentpuldur<=pulse_durations[pulse_index];
-			if(pulse_index == 15) pulse_index <= 0;
+			if(pulse_index == 7) pulse_index <= 0;
 			else pulse_index <= pulse_index + 1'd1;
 			timer <= 0;			
 		end
--- soft_i2c_cyclone10gx/soft_i2c_cyclone10gx.ino
+++ soft_i2c_cyclone10gx/soft_i2c_cyclone10gx.ino
@@ -1,5 +1,43 @@
-#include <XantoI2C.h>
 
+/*
+Board: Arduino Nano
+Processor: ATmega168
+*/
+
+#include <Wire.h>
+byte CA = 0x30;
+unsigned createMask(unsigned a, unsigned b){
+   unsigned r = 0;
+   for (unsigned i=a; i<=b; i++) r |= 1 << i;
+   return r;
+}
+
+//chip=0 is for pulses (DAC 0 =P1, 1=P2, 2=P3, 3=P4, 4=N1, 5=N2, 6=N3, 7=N4)
+//chip=1 is for offsets and unused extras (DAC 0 =offset1, 1=offset2, 2=extra3, 3=4, 4=5, 5=6, 6=7, 7=8)
+//DAC is which of the 8 DACs on the chip to alter
+//value is 0 -- 4095 (12 bit) where 0 is 0V and 4095 is the input voltage (1.8V), and linear in between, e.g. 2048 = 1.8V/2
+void writeDAC(uint16_t value, uint8_t DAC, uint8_t chip){
+  uint16_t MSDB2, LSDB2;
+  
+  unsigned r = createMask(0, 3);
+  LSDB2 = value & r;
+  LSDB2 = LSDB2 << 4;
+  
+  unsigned x = createMask(3, 11);
+  MSDB2 = value & x;
+  MSDB2 = MSDB2 >> 4;
+  
+  Wire.begin();
+  // B1001100 0x4C floating , B1001010 0x4A vcc, B1001000 0x48 gnd
+  if (chip==0) Wire.beginTransmission(0x4C);
+  if (chip==1) Wire.beginTransmission(0x48);
+  Wire.write(CA + DAC);    //CA byte
+  Wire.write(MSDB2);  //MSDB
+  Wire.write(LSDB2);  //LSDB
+  Wire.endTransmission();
+}
+
+#include <XantoI2C.h>
 const uint8_t PIN_SCL = 9;
 const uint8_t PIN_SDA = 11;
 
@@ -57,6 +95,25 @@ void setup() {
   delay(1000);
   sendOutputCommandOn();
   delay(100);
+
+//  writeDAC(500, 0, 0); // pulse amplitudes
+//  writeDAC(0, 1, 0);
+//  writeDAC(0, 2, 0);
+//  writeDAC(0, 3, 0);
+//  writeDAC(0, 4, 0);
+//  writeDAC(0, 5, 0);
+//  writeDAC(0, 6, 0);
+//  writeDAC(0, 7, 0);
+//  
+//  writeDAC(0, 0, 1); // offset1 adjust
+//  writeDAC(0, 1, 1); // offset2 adjust
+//  
+//  writeDAC(0, 2, 1); // extras
+//  writeDAC(0, 3, 1);
+//  writeDAC(0, 4, 1);
+//  writeDAC(0, 5, 1);
+//  writeDAC(0, 6, 1);
+//  writeDAC(0, 7, 1);
 }
 
 void loop() {
